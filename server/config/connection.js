deeb5e950b4ae7cbae3921833243a010c4ba8e11
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 11:13:11 -0800

    changed db to mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://xenonxpo:Aegis123@cluster0.wef9uht.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
