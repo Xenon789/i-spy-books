796028fb5f4a04189409343d7c86b5eb72fb6c34
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 11:34:26 -0800

    make it better

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://xenonxpo:Aegis123@cluster0.wef9uht.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://xenonxpo:Aegis123@cluster0.wef9uht.mongodb.net/googlebooks?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
